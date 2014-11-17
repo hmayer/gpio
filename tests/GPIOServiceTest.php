a840e93050216286aff7ded883800e8a00b09b05
Michael <wafflestealer654@users.noreply.github.com>
2014-11-17 17:11:50 -0500

    Update GPIOServiceTest.php

--- tests/GPIOServiceTest.php
+++ tests/GPIOServiceTest.php
@@ -1,7 +1,17 @@
 <?php
 namespace WaffleSystems\Tests\GPIO;
 
+use WaffleSystems\GPIO\GPIOService;
+use WaffleSystems\GPIO\GPIOServiceFactory;
+
 class GPIOServiceTest extends \PHPUnit_Framework_TestCase
 {
+
+    public function testRaspberryPiFactory()
+    {
+        $factory = GPIOServiceFactory::createForRaspberryPi();
+        $this->assertTrue($factory instanceof GPIOService);
+        $this->assertTrue($factory->getHandler() instanceof RaspberryPiGPIOInterface);
+    }
 
 }
